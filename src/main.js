0a7e8bd97e9b3af1ea2b7c59c12f44b6eca2923d
Daniel Noon <supercookies@outlook.com>
2018-04-15 12:47:36 -0500

    wow

--- src/main.js
+++ src/main.js
@@ -1,11 +1,78 @@
-import _ from 'lodash';
+import md5 from 'md5';
+import marked from 'marked';
 
-function component() {
-  let element = document.createElement('div');
+const theme = "light";
+const robomail = "seminar.robot@supermegadex.cf";
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+console.log("I'm here...");
+const messageList = select('#message-list');
+const messages = [
+  {
+    author: {
+      name: "Daniel",
+      email: "delpinothedragon1@hotmail.com"
+    },
+    text: "This is some text"
+  },
+  {
+    author: {
+      name: "Ben",
+      email: "benwingerter01@gmail.com"
+    },
+    text: "This is some more text"
+  }
+];
 
-  return element;
+function select(selector) {
+  return document.querySelector(selector);
 }
 
-document.body.appendChild(component());
+function create(name, props) {
+  let el = document.createElement(name);
+  const attrs = Object.keys(props);
+  for (let attr of attrs) {
+    el[attr] = props[attr];
+  }
+  return el;
+}
+
+function makeChannelText(message) {
+  let el = create("div", {
+    className: "fluent-item"
+  });
+  if (message.author.email) {
+    el.appendChild(create("img", {
+      className: "fluent-avatar",
+      src: getGravatarImage(message.author.email)
+    }));
+  }
+  let md = `<small class='name'>${message.author.name}</small>` + marked(message.text);
+  el.appendChild(create('div', {
+    className: "fluent-item-inner padding",
+    innerHTML: md
+  }));
+  return el;
+}
+
+function update() {
+  const els = [];
+  console.log(messageList);
+  for (let message of messages) {
+    els.push(makeChannelText(message));
+  }
+  messageList.innerHTML = "";
+  for (let el of els) {
+    messageList.appendChild(el);
+  }
+}
+
+update();
+  
+function getGravatarImage(email) {
+  return `https://www.gravatar.com/avatar/${md5(email)}?d=retro`;
+}
+
+(function () {
+  const tabs = document.querySelectorAll('.fluent-list.tabs');
+  
+})();
